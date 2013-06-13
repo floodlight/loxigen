9dc561994c987e86a9d2266a7058b40fd3d9b37f
Rich Lane <rlane@bigswitch.com>
2013-06-13 11:52:33 -0700

    add pointer to license files to loci/pyloxi generated copyright headers
    This indirection allows the project containing loci/pyloxi to pick the license.

--- c_gen/templates/_copyright.c
+++ c_gen/templates/_copyright.c
@@ -28,3 +28,4 @@
 /* Copyright (c) 2008 The Board of Trustees of The Leland Stanford Junior University */
 /* Copyright (c) 2011, 2012 Open Networking Foundation */
 /* Copyright (c) 2012, 2013 Big Switch Networks, Inc. */
+/* See the file LICENSE.loci which should have been included in the source distribution */
