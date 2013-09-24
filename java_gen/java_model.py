376cef52790f7016ef017e12716b5977efe7b841
Andreas Wundsam <andreas.wundsam@bigswitch.com>
2013-09-24 13:52:18 -0700

    java_gen: add MaskedEnumGroup fro OFTableConfig.TABLE_MISS
    remove unused var

--- java_gen/java_model.py
+++ java_gen/java_model.py
@@ -159,7 +159,10 @@ class JavaModel(object):
             OFPortState = (MaskedEnumGroup("stp_flags", mask="STP_MASK", members=set(("STP_LISTEN", "STP_LEARN", "STP_FORWARD", "STP_BLOCK"))), ),
             OFConfigFlags = (
                 MaskedEnumGroup("frag_flags", mask="FRAG_MASK", members=set(("FRAG_NORMAL", "FRAG_DROP", "FRAG_REASM"))),
-            )
+            ),
+            OFTableConfig = (
+                MaskedEnumGroup("table_miss_flags", mask="TABLE_MISS_MASK", members=set(("TABLE_MISS_CONTROLLER", "TABLE_MISS_CONTINUE", "TABLE_MISS_DROP"))),
+            ),
     )
 
     # represents a metadata property associated with an EnumClass
