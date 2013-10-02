4b9e306679bfcf544cdec17a3dd32eb8a29ff38f
Vishnu Emmadi <vishnu.emmadi@bigswitch.com>
2013-10-01 17:10:18 -0700

    add slot_num

--- c_gen/templates/loci_show.h
+++ c_gen/templates/loci_show.h
@@ -339,5 +339,6 @@ int of_object_show(loci_writer_f writer, void *cookie, of_object_t *obj);
 #define LOCI_SHOW_u16_hybrid_version(writer, cookie, val) LOCI_SHOW_u16(writer, cookie, val)
 #define LOCI_SHOW_u32_timeout_ms(writer, cookie, val) LOCI_SHOW_u32(writer, cookie, val)
 #define LOCI_SHOW_u32_tx_interval_ms(writer, cookie, val) LOCI_SHOW_u32(writer, cookie, val)
+#define LOCI_SHOW_u8_slot_num(writer, cookie, val) LOCI_SHOW_u8(writer, cookie, val)
 
 #endif /* _LOCI_SHOW_H_ */
