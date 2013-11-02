771b08abf572c7eece861deeeab58b991687a7bf
Tomasz <tomasz.klimczyk@bigswitch.com>
2013-11-02 13:33:57 -0700

    Improving of_stats messages reading

--- wireshark_gen/templates/openflow.lua
+++ wireshark_gen/templates/openflow.lua
@@ -179,7 +179,13 @@ function dissect_of_message(buf, root)
     end
 
     local info = "unknown"
-    if of_message_dissectors[version_val] and of_message_dissectors[version_val][type_val] then
+    if type_val == 19 then
+        local stats_type = buf(8,2):uint()
+        info = of_stats_reply_dissectors[version_val][stats_type](reader,subtree)
+    elseif type_val == 18 then
+        local stats_type = buf(8,2):uint()
+        info = of_stats_request_dissectors[version_val][stats_type](reader,subtree)
+    elseif of_message_dissectors[version_val] and of_message_dissectors[version_val][type_val] then
         info = of_message_dissectors[version_val][type_val](reader, subtree)
     end
 
