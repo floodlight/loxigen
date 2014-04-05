dc9bc7f8aa4f2e2477033b686d4c97612a3f2efb
Rich Lane <rlane@bigswitch.com>
2014-04-05 11:19:20 -0700

    loci: lazily zero wire buffer
    Previously we zeroed the entire wire buffer when it was allocated. This was
    wasteful because often the message didn't grow to fill the entire allocated
    size, and the memset was consuming 80% of CPU time during one of my benchmarks.
    
    With this change, we only zero memory right before giving it to the user.
    Additionally, of_octets_t fields like packet-in data aren't zeroed at all.

--- c_gen/templates/of_wire_buf.h
+++ c_gen/templates/of_wire_buf.h
@@ -174,7 +174,6 @@ of_wire_buffer_new(int a_bytes)
         FREE(wbuf);
         return NULL;
     }
-    MEMSET(wbuf->buf, 0, a_bytes);
     wbuf->current_bytes = 0;
     wbuf->alloc_bytes = a_bytes;
 
@@ -245,6 +244,7 @@ of_wire_buffer_grow(of_wire_buffer_t *wbuf, int bytes)
     LOCI_ASSERT(wbuf != NULL);
     LOCI_ASSERT(wbuf->alloc_bytes >= bytes);
     if (bytes > wbuf->current_bytes) {
+        MEMSET(wbuf->buf + wbuf->current_bytes, 0, bytes - wbuf->current_bytes);
         wbuf->current_bytes = bytes;
     }
 }
