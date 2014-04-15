dd53516f333e283f26d1a26a73c44303d3d59638
KC Wang <kc.wang@bigswitch.com>
2014-04-14 22:37:22 -0700

    address review comment

--- java_gen/pre-written/src/main/java/org/projectfloodlight/openflow/types/VlanVid.java
+++ java_gen/pre-written/src/main/java/org/projectfloodlight/openflow/types/VlanVid.java
@@ -33,7 +33,9 @@ public class VlanVid implements OFValueType<VlanVid> {
     }
 
     public static VlanVid ofVlan(int vid) {
-        if ((vid & VALIDATION_MASK) != vid && vid != NO_MASK.vid)
+        if (vid == NO_MASK.vid)
+            return NO_MASK;
+        if ((vid & VALIDATION_MASK) != vid)
             throw new IllegalArgumentException(String.format("Illegal VLAN value: %x", vid));
         return new VlanVid((short) vid);
     }
