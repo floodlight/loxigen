8c9d9575d3db698453326c0c1c3e43e62e04865f
Wilson Ng <wilson.ng@bigswitch.com>
2014-04-15 16:27:53 -0700

    Add flags to bsn_vport_l2gre.

--- c_gen/templates/loci_show.h
+++ c_gen/templates/loci_show.h
@@ -406,5 +406,6 @@ int of_object_show(loci_writer_f writer, void *cookie, of_object_t *obj);
 #define LOCI_SHOW_u8_dscp(writer, cookie, val) LOCI_SHOW_u8(writer, cookie, val)
 #define LOCI_SHOW_u8_ttl(writer, cookie, val) LOCI_SHOW_u8(writer, cookie, val)
 #define LOCI_SHOW_u32_vpn(writer, cookie, val) LOCI_SHOW_u32(writer, cookie, val)
+#define LOCI_SHOW_u32_flags(writer, cookie, val) LOCI_SHOW_u32(writer, cookie, val)
 
 #endif /* _LOCI_SHOW_H_ */
