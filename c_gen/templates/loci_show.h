fc0650390ed824e90bf9b84c6b2df65b001e1b52
Vishnu Emmadi <vishnu.emmadi@bigswitch.com>
2014-04-17 15:03:18 -0700

    rename fields in bsn_image
    port Rich's fix in loci to support stats type for image_desc_stats

--- c_gen/templates/loci_show.h
+++ c_gen/templates/loci_show.h
@@ -398,7 +398,6 @@ int of_object_show(loci_writer_f writer, void *cookie, of_object_t *obj);
 #define LOCI_SHOW_u64_counter_id(writer, cookie, val) LOCI_SHOW_u64(writer, cookie, val)
 #define LOCI_SHOW_desc_str_description(writer, cookie, val) LOCI_SHOW_desc_str(writer, cookie, val)
 #define LOCI_SHOW_str64_name(writer, cookie, val) LOCI_SHOW_str64(writer, cookie, val)
-#define LOCI_SHOW_u32_image_checksum_hash(writer, cookie, val) LOCI_SHOW_u32(writer, cookie, val)
-#define LOCI_SHOW_u32_startup_config_hash(writer, cookie, val) LOCI_SHOW_u32(writer, cookie, val)
-
+#define LOCI_SHOW_desc_str_image_checksum(write, cookie, val) LOCI_SHOW_desc_str(writer, cookie, val)
+#define LOCI_SHOW_desc_str_startup_config_checksum(write, cookie, val) LOCI_SHOW_desc_str(writer, cookie, val)
 #endif /* _LOCI_SHOW_H_ */
