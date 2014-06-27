a5eeae36c29feab2d777b1df89a440e4579f5769
Rich Lane <rlane@bigswitch.com>
2014-06-26 19:59:04 -0700

    add bsn_vlan_xlate_port_group_id extension

--- java_gen/java_type.py
+++ java_gen/java_type.py
@@ -647,6 +647,9 @@ exceptions = {
         'of_oxm_bsn_tcp_flags' : { 'value' : u16obj },
         'of_oxm_bsn_tcp_flags_masked' : { 'value' : u16obj, 'value_mask' : u16obj },
 
+        'of_oxm_bsn_vlan_xlate_port_group_id' : { 'value' : class_id },
+        'of_oxm_bsn_vlan_xlate_port_group_id_masked' : { 'value' : class_id, 'value_mask' : class_id },
+
         'of_table_stats_entry': { 'wildcards': table_stats_wildcards },
         'of_match_v1': { 'vlan_vid' : vlan_vid_match, 'vlan_pcp': vlan_pcp,
                 'eth_type': eth_type, 'ip_dscp': ip_dscp, 'ip_proto': ip_proto,
