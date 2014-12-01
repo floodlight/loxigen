119289c9b2404b6819119c5b08c0b4d15ac69c96
Rich Lane <rlane@bigswitch.com>
2014-12-01 13:44:08 -0800

    loci: support of_bitmap_512 basic type

--- c_gen/c_test_gen.py
+++ c_gen/c_test_gen.py
@@ -105,6 +105,7 @@ def var_name_map(m_type):
         # BSN extensions
         of_bsn_vport_q_in_q_t="vport",
         of_bitmap_128_t="bitmap_128",
+        of_bitmap_512_t="bitmap_512",
         of_checksum_128_t="checksum_128",
         )
 
@@ -120,7 +121,7 @@ integer_types = ["uint8_t", "uint16_t", "uint32_t", "uint64_t",
 string_types = [ "of_port_name_t", "of_table_name_t",
                 "of_desc_str_t", "of_serial_num_t", "of_mac_addr_t",
                 "of_ipv6_t", "of_bitmap_128_t", "of_checksum_128_t",
-                "of_str64_t"]
+                "of_str64_t", "of_bitmap_512_t"]
 
 scalar_types = integer_types[:]
 scalar_types.extend(string_types)
