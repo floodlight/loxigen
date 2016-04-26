ae75d35b0af264e37dfb21d3e90e7e59ad24a9cb
Andreas Wundsam <andreas.wundsam@bigswitch.com>
2016-04-26 13:11:53 -0700

    java/OFConnectionIndex: add toString/equals/hashcode

--- java_gen/pre-written/src/main/java/org/projectfloodlight/openflow/types/OFConnectionIndex.java
+++ java_gen/pre-written/src/main/java/org/projectfloodlight/openflow/types/OFConnectionIndex.java
@@ -1,18 +1,18 @@
 package org.projectfloodlight.openflow.types;
 
-import io.netty.buffer.ByteBuf;
 import org.projectfloodlight.openflow.annotations.Immutable;
-import org.projectfloodlight.openflow.exceptions.OFParseError;
 
 import com.google.common.hash.PrimitiveSink;
 import com.google.common.primitives.UnsignedInts;
+
+import io.netty.buffer.ByteBuf;
 
 @Immutable
 public class OFConnectionIndex implements OFValueType<OFConnectionIndex>  {
 
     static final int LENGTH = 4;
     private final int connectionIndex;
-    
+
     private OFConnectionIndex(final int connectionIndex) {
         this.connectionIndex = connectionIndex;
     }
@@ -20,7 +20,7 @@ public class OFConnectionIndex implements OFValueType<OFConnectionIndex>  {
     public static OFConnectionIndex of(final int connectionIndex) {
         return new OFConnectionIndex(connectionIndex);
     }
-    
+
     public void write4Bytes(ByteBuf c) {
         c.writeInt(connectionIndex);
     }
@@ -28,7 +28,7 @@ public class OFConnectionIndex implements OFValueType<OFConnectionIndex>  {
     public static OFConnectionIndex read4Bytes(ByteBuf c) {
         return OFConnectionIndex.of(c.readInt());
     }
-    
+
     @Override
     public OFConnectionIndex applyMask(OFConnectionIndex mask) {
         return OFConnectionIndex.of(this.connectionIndex & mask.connectionIndex);
@@ -42,10 +42,37 @@ public class OFConnectionIndex implements OFValueType<OFConnectionIndex>  {
     @Override
     public void putTo(PrimitiveSink sink) {
         sink.putInt(connectionIndex);
-    }   
-    
+    }
+
     @Override
     public int getLength() {
         return LENGTH;
+    }
+
+    @Override
+    public String toString() {
+        return UnsignedInts.toString(connectionIndex);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + connectionIndex;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OFConnectionIndex other = (OFConnectionIndex) obj;
+        if (connectionIndex != other.connectionIndex)
+            return false;
+        return true;
     }
 }
