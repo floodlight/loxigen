68a4bc6bf7af4404bc4651d81b91aba03bcb8521
Ryan Izard <ryan.izard@bigswitch.com>
2016-06-21 08:30:50 -0400

    Need to expose equalsIgnoreXid() in OFMessage, noting that any implementer of OFMessage will/should have an XID. This will avoid having to cast the OFMessage to the version-specific class in order to access the function.

---
+++ java_gen/templates/custom/interface/OFMessage.java
@@ -0,0 +1,9 @@
+    // Additional methods
+    
+    /**
+     * Compares the two messages for equality, ignoring the XID field.
+     *
+     * @param obj the other message to compare
+     * @return true if the messages are equal, ignoring the XID; false otherwise
+     */
+    boolean equalsIgnoreXid(Object obj);
