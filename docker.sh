6bccf7eade2f9f373ef5f48d68589471399b7854
Andreas Wundsam <andreas.wundsam@bigswitch.com>
2017-11-29 15:14:33 +0000

    docker.sh: add option to add docker opts via env var DOCKER_OPTS

--- docker.sh
+++ docker.sh
@@ -4,5 +4,6 @@ if [ -t 1 ]; then
 else
     tty_flag=""
 fi
+
 dir=$(python -c 'import os; import sys; print os.path.abspath(sys.argv[1])' $(dirname "$0"))
-docker run -i $tty_flag --rm -v $dir:/loxi floodlight/loxi-builder-ubuntu14 "$@"
+docker run -i $tty_flag --rm -v $dir:/loxi $DOCKER_OPTS floodlight/loxi-builder-ubuntu14 "$@"
